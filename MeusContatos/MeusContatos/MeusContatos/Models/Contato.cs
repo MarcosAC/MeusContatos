3a040062bc61acd7213bd6d161c7a3a89df96504
marcos.aucorrea@gmail.com <marcos.aucorrea@gmail.com>
2018-08-15 15:14:39 -0300

    Models.

--- MeusContatos/MeusContatos/MeusContatos/Models/Contato.cs
+++ MeusContatos/MeusContatos/MeusContatos/Models/Contato.cs
@@ -1,7 +1,11 @@
-﻿namespace MeusContatos.Models
+﻿using SQLite;
+
+namespace MeusContatos.Models
 {
+    [Table("Contato")]
     public class Contato
     {
+        [PrimaryKey, AutoIncrement]
         public int IdContato { get; set; }
         public string NomeContato { get; set; }
         public Telefone Telefone { get; set; }
--- MeusContatos/MeusContatos/MeusContatos/Models/Telefone.cs
+++ MeusContatos/MeusContatos/MeusContatos/Models/Telefone.cs
@@ -1,7 +1,11 @@
-﻿namespace MeusContatos.Models
+﻿using SQLite;
+
+namespace MeusContatos.Models
 {
+    [Table("Telefone")]
     public class Telefone
     {
+        [PrimaryKey, AutoIncrement]
         public int IdTelefone { get; set; }
         public string Celular { get; set; }
         public string TefoneFixo { get; set; }
