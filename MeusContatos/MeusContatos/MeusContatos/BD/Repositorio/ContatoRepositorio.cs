53fabe76c85192d89b3d0c3996e4ce9d7ec7c968
marcos.aucorrea@gmail.com <marcos.aucorrea@gmail.com>
2018-08-15 16:27:02 -0300

    ContatoRepositorio.

---
+++ MeusContatos/MeusContatos/MeusContatos/BD/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using MeusContatos.Models;
+using System.Collections.Generic;
+
+namespace MeusContatos.BD.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        DataBaseService _dataBaseService;
+
+        public ContatoRepositorio()
+        {
+            _dataBaseService = new DataBaseService();
+        }
+
+        public void AdicionarContato(Contato contato)
+        {
+            _dataBaseService.AdicionarContato(contato);
+        }
+
+        public void DeletarContato(int contatoId)
+        {
+            _dataBaseService.DeletarContato(contatoId);
+        }
+
+        public void DeletarTodosContatos()
+        {
+            _dataBaseService.DeletarTodosContatos();
+        }
+
+        public void EditarContato(Contato contato)
+        {
+            _dataBaseService.EditarContato(contato);
+        }
+
+        public Contato ObterContato(int contatoId)
+        {
+            return _dataBaseService.ObterContato(contatoId);
+        }
+
+        public List<Contato> ObterTodosContat()
+        {
+            return _dataBaseService.ObterTodosContatos();
+        }
+    }
+}
