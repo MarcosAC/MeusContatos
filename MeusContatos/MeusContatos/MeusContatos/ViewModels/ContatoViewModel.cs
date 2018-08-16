03aa3784bb4e817eb2171f50541ce6d64f4f92e6
marcos.aucorrea@gmail.com <marcos.aucorrea@gmail.com>
2018-08-16 15:00:59 -0300

    ContatoViewModel

---
+++ MeusContatos/MeusContatos/MeusContatos/ViewModels/ContatoViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeusContatos.ViewModels
+{
+    public class ContatoViewModel : BaseViewModel
+    {
+        public ContatoViewModel()
+        {
+
+        }
+
+        private string _NomeContato;
+        public string NomeContato
+        {
+            get { return _NomeContato; }
+            set
+            {
+                _NomeContato = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Celular;
+        public string Celular
+        {
+            get { return _Celular; }
+            set
+            {
+                _Celular = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _TelefoneFixo;
+        public string TelefoneFixo
+        {
+            get { return _TelefoneFixo; }
+            set
+            {
+                _TelefoneFixo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
