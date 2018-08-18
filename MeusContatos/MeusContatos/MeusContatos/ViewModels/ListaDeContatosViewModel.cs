bf31824f69b6019d9aa585424307ccf016fdd1d6
marcos.aucorrea@gmail.com <marcos.aucorrea@gmail.com>
2018-08-18 00:43:25 -0300

    Lista de Contatos ok.

--- MeusContatos/MeusContatos/MeusContatos/ViewModels/ListaDeContatosViewModel.cs
+++ MeusContatos/MeusContatos/MeusContatos/ViewModels/ListaDeContatosViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MeusContatos.Views;
+﻿using MeusContatos.BD.Repositorio;
+using MeusContatos.Models;
+using MeusContatos.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,13 +8,26 @@ using Xamarin.Forms;
 
 namespace MeusContatos.ViewModels
 {
-    public class ListaDeContatosViewModel : BaseViewModel
+    public class ListaDeContatosViewModel : ContatoViewModel
     {
+        private readonly IContatoRepositorio _contatoRepositorio;
+
+        public List<Contato> listaDeContatos { get; }
+
         public Command IrParaCadastroContatoCommand { get; }
 
         public ListaDeContatosViewModel()
         {
+            _contatoRepositorio = new ContatoRepositorio();
+
+            listaDeContatos = ListaDeContatos();
+
             IrParaCadastroContatoCommand = new Command(ExecuteIrParaCadastroContatoCommand);
+        }
+
+        private List<Contato> ListaDeContatos()
+        {
+            return _contatoRepositorio.ObterTodosContatos();
         }
 
         private async void ExecuteIrParaCadastroContatoCommand()
--- MeusContatos/MeusContatos/MeusContatos/Views/CadastroContatoView.xaml.cs
+++ MeusContatos/MeusContatos/MeusContatos/Views/CadastroContatoView.xaml.cs
@@ -1,18 +1,17 @@
-﻿using MeusContatos.Models;
-using MeusContatos.ViewModels;
+﻿using MeusContatos.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MeusContatos.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CadastroContatoView : ContentPage
-    {
-        public CadastroContatoView()
-        {
-            InitializeComponent();
+	public partial class CadastroContatoView : ContentPage
+	{
+		public CadastroContatoView()
+		{
+			InitializeComponent();
 
             BindingContext = new ContatoViewModel();
-        }
-    }
+		}
+	}
 }
